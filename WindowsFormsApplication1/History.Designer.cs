7ff5a7f197bd9057caa99137055f33ebf14f0ab2
lena <islenaliny@gmail.com>
2020-07-22 17:01:49 +0800

    First version

---
+++ WindowsFormsApplication1/History.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class History
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvHistory = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvHistory
+            // 
+            this.dgvHistory.AllowUserToAddRows = false;
+            this.dgvHistory.AllowUserToDeleteRows = false;
+            this.dgvHistory.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvHistory.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistory.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dgvHistory.Location = new System.Drawing.Point(12, 12);
+            this.dgvHistory.Name = "dgvHistory";
+            this.dgvHistory.ReadOnly = true;
+            this.dgvHistory.RowHeadersVisible = false;
+            this.dgvHistory.RowTemplate.Height = 24;
+            this.dgvHistory.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvHistory.Size = new System.Drawing.Size(570, 267);
+            this.dgvHistory.TabIndex = 3;
+            // 
+            // History
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 292);
+            this.Controls.Add(this.dgvHistory);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "History";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "大樂透各期開獎結果";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvHistory;
+
+    }
+}
---
+++ WindowsFormsApplication1/PickNumber.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class PickNumber : Form
+    {
+        int iSelectedCnt = 0;
+
+        public PickNumber()
+        {
+            InitializeComponent();
+
+            IniControl();
+
+            this.btnSubmit.Click += new EventHandler(Button_Click);
+        }
+
+        private void IniControl() 
+        {
+           
+            int iCnt = 0;
+            for (int intA = 0; intA < 7; intA++)
+            {
+                for (int intB = 0; intB < 7; intB++)
+                {
+                    Button button = new Button();
+                    iCnt++;
+                    button.Name = "btn" + iCnt.ToString().PadLeft(2, '0');
+                    button.Text = iCnt.ToString().PadLeft(2, '0');
+                    button.Size = new Size(30, 30);
+                    button.Margin = new Padding(3, 3, 3, 3);
+                    button.Location = new Point((9 * (intB * 4 + 1)), (9 * (intA * 4 + 1)));
+                    button.BackColor = Color.White;
+                    button.FlatStyle = FlatStyle.Flat;
+                    button.Click += (sender, e) => BtnNumber_Click(sender, e); 
+
+                    this.panel1.Controls.Add(button);
+                   
+                }
+            }
+        
+            this.lblWarm.Text = string.Format("還有{0}個數字", iSelectedCnt);
+        
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (sender.Equals(this.btnSubmit))
+            {
+                Main frm1 = (Main)this.Owner;   //将本窗体的拥有者强制设为Main类的实例frm1
+                Random rnd = new Random();
+                int iRnd = 0;
+                int index = iSelectedCnt;
+                string sSelectedText = this.lblSelected.Text;
+                
+                for (int intA = 0; intA < this.nudAmount.Value; intA++)
+                {
+                    while (index < 6)
+                    {
+                        iRnd = rnd.Next(1, 50);
+                        if (!sSelectedText.Contains(iRnd.ToString()))
+                        {
+                            sSelectedText = sSelectedText + iRnd.ToString().PadLeft(2, '0') + "　";
+                            index++;
+                        }
+                    }
+
+                    frm1.AddPickNumber(sSelectedText.Trim());
+                    index = iSelectedCnt;
+                    sSelectedText = this.lblSelected.Text;
+                }
+
+                this.Close();
+            }
+        }
+
+        private void BtnNumber_Click(object sender, EventArgs e) 
+        {
+            Button btn = (Button)sender;
+
+            if (btn.BackColor == Color.White)
+            {
+                iSelectedCnt++;
+                if (!CheckNumber()) return;
+                
+                btn.BackColor = Color.DarkGray;
+                this.lblSelected.Text = this.lblSelected.Text + btn.Text + "　";
+                
+            }
+            else if (btn.BackColor == Color.DarkGray)
+            {
+                iSelectedCnt--;
+                if (!CheckNumber()) return;
+
+                btn.BackColor = Color.White;
+                this.lblSelected.Text = this.lblSelected.Text.Replace(btn.Text + "　", "");
+                
+            }
+        }
+
+        private bool CheckNumber() 
+        {
+            if (iSelectedCnt > 6 || iSelectedCnt < 0)
+            {
+                MessageBox.Show("最多只能選6個號碼", this.Text);
+                iSelectedCnt = 6;
+                return false;
+            }
+
+            this.lblWarm.Text = string.Format("還有{0}個數字", 6 - iSelectedCnt);
+
+            return true;
+        }
+    }
+}
